1c8fd6bef345236d91ec2d33d66a47e4d21d08bc
Jianjian-18 <zenghuajian97@gmail.com>
2022-08-15 17:08:06 +0200

    minor change

--- rl-ros-agents/setup.py
+++ rl-ros-agents/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['rl-ros-agents']
+    packages=['rl_ros_agents']
 )
 
 setup(**setup_args)
